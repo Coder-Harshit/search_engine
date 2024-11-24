75ddf5a7f20ea086661fad8dec7dd50035995cfa
Harshit Vijay <93333205+Coder-Harshit@users.noreply.github.com>
2024-11-24 13:51:11 +0530

    Added vercel speed-insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import dynamic from 'next/dynamic'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Suspense } from 'react'
 
 const DynamicHome = dynamic(() => import('@/components/Home'), {
@@ -10,6 +11,7 @@ const DynamicHome = dynamic(() => import('@/components/Home'), {
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      <SpeedInsights />
       <DynamicHome />
     </Suspense>
   )
